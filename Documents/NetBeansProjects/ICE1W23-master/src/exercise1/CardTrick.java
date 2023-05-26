63a7316b0768d8fd4447a9807b3879b90dfea863
82108 <doh6077@gmail.com>
2023-05-26 14:05:29 -0400

    added new codes

--- Documents/NetBeansProjects/ICE1W23-master/src/exercise1/CardTrick.java
+++ Documents/NetBeansProjects/ICE1W23-master/src/exercise1/CardTrick.java
@@ -1,19 +1,22 @@
 package exercise1;
-
+import java.util.Scanner; 
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
  *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+
+ * @author Dohee Kim May, 26, 2023
  */
 public class CardTrick {
     
     public static void main(String[] args) {
-        
-        Card[] hand = new Card[7];
+        Scanner input = new Scanner(System.in);
+        System.out.println("pick a card");
+        int num = input.nextInt();
 
+        Card[] hand = new Card[7];
+  
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
             //card.setValue(insert call to random number generator here)
