7b8f8ff200c616fe12e384c18405986112d75d9d
doh6077 <134092191+doh6077@users.noreply.github.com>
2023-05-26 14:22:48 -0400

    Update CardTrick.java
    changed CardTrick.java

--- Documents/NetBeansProjects/ICE1W23-master/src/exercise1/CardTrick.java
+++ Documents/NetBeansProjects/ICE1W23-master/src/exercise1/CardTrick.java
@@ -1,5 +1,6 @@
 package exercise1;
 import java.util.Scanner; 
+import java.util.Rnadom; 
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
@@ -16,9 +17,14 @@ public class CardTrick {
         int num = input.nextInt();
 
         Card[] hand = new Card[7];
+	Random random  = new Random(); 
   
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
+	    int suitIndex = random.nextInt(suits.length);
+            int rankIndex = random.nextInt(ranks.length);
+            hand[i] = ranks[rankIndex] + " of " + suits[suitIndex];
+
             //card.setValue(insert call to random number generator here)
             // 
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
@@ -35,7 +41,12 @@ public class CardTrick {
         // Then loop through the cards in the array to see if there's a match.
         
         // If the guess is successful, invoke the printInfo() method below.
-        
+            for (Card card : hand) {
+            if (card.getValue() == num && card.getSuit().equals(suit)) {
+                printInfo();
+                break;
+            }
+
     }
 
     /**
@@ -48,19 +59,16 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Dohee");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- Participate in classes actively");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Working out");
+        System.out.println("-- Reading Books");
 
         System.out.println();
         
