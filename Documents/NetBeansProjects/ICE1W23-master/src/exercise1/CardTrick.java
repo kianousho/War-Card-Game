3fb275272ab12f21494ee418f0b3068455d18922
doh6077 <134092191+doh6077@users.noreply.github.com>
2023-05-29 20:34:58 -0400

    Modified the printinfo()
    Modified the printinfo()

--- Documents/NetBeansProjects/ICE1W23-master/src/exercise1/CardTrick.java
+++ Documents/NetBeansProjects/ICE1W23-master/src/exercise1/CardTrick.java
@@ -67,19 +67,16 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Dohee");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- Improve Sofe Skills");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
         System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Working out");
 
         System.out.println();
         
