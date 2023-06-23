8f35f24a86b480dfdd3ac6e2bf750847a2d691af
82108 <doh6077@gmail.com>
2023-06-23 12:49:09 -0400

    Create a class called TestUserprofile

---
+++ Documents/NetBeansProjects/MidTermPartC-master/src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author 82108
+ */
+public class TestUserprofile {
+    public void main(String[] args){
+        UserProfile userPro = new UserProfile("doheeKim","Comedy");
+        System.out.println("Your userProfile was created");
+    }
+    
+}
